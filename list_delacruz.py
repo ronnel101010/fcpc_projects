de5f5e0af6398fbd7cf74329808cebbfbec5df36
ronnel101010 <45375242+ronnel101010@users.noreply.github.com>
2018-11-27 09:55:13 +0800

    Create list_delacruz.py

---
+++ list_delacruz.py
@@ -0,0 +1,17 @@
+
+mga_aso = ["whitey","brownie","blackie"]
+mga_numero = [1,2,3,4,5,6]
+mga_decimal_points = [1.0,1.2,1.3,1.4]
+
+for x in range (len(mga_aso)):
+    print(mga_aso[x])
+
+for y in range(len(mga_numero)):
+    print(mga_numero[y])
+
+for z in range(len(mga_decimal_points)):
+    print(mga_decimal_points[z])
+
+print(len(mga_aso))
+print(len(mga_numero))
+print(len(mga_decimal_points))
